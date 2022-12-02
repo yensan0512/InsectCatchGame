54849b22b5018b541d5f5f2ea86231c4675511ad
yensan0512 <yensan1254@gmail.com>
2022-12-02 23:20:23 +0800

    'first'

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-const  panels = document.querySelectorAll('.panel')
-console.log(panels[0])
-console.log(panels)
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-panels.forEach((panel) =>{
-    panel.addEventListener('click',() => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+let currentActive = 1
+
+next.addEventListener('click',() => {
+    currentActive++
+
+    if(currentActive > circles.length){
+        currentActive = circles.length
+    }
+
+    update()
+    
+    console.log(currentActive)
 })
 
-function removeActiveClasses(){
-    panels.forEach(panel => {
-        panel.classList.remove('active')
+prev.addEventListener('click',() => {
+    currentActive--
+
+    if(currentActive < 1){
+        currentActive = 1
+    }
+
+    update()
+    
+    console.log(currentActive)
+})
+
+
+function update(){
+    circles.forEach((circle,idx)=>{
+        if(idx < currentActive){
+            circle.classList.add('active')
+        }
+        else{
+            circle.classList.remove('active')
+        }
+
     })
+
+    const actives = document.querySelectorAll('.active')
+
+    console.log((actives.length - 1)/ (circles.length - 1)* 100)
+
+    progress.style.width = ((actives.length - 1)/ (circles.length - 1)* 100) + '%'
+
+    if(currentActive === 1){
+        prev.disabled = true
+    }else if(currentActive === circles.length){
+        next.disabled = true
+    }else{
+        prev.disabled = false
+        next.disabled = false
+    }
 }
