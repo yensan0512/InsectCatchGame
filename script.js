747c8259f2c721653a6bd0abb3c8a2426d0e2566
yensan0512 <yensan1254@gmail.com>
2022-12-03 00:44:09 +0800

    'first'

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
 
-open.addEventListener('click',() => container.classList.add('show-nav'))
-
-close.addEventListener('click',() => container.classList.remove('show-nav'))
+btn.addEventListener('click',()=>{
+    search.classList.toggle('active')
+    input.focus()
+})
