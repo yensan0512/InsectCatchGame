ce3889997a9d0daa6b1d3f728a277468c5d763f1
yensan0512 <yensan1254@gmail.com>
2022-12-03 00:26:19 +0800

    'first'

--- script.js
+++ script.js
@@ -1,58 +1,7 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
-let currentActive = 1
+open.addEventListener('click',() => container.classList.add('show-nav'))
 
-next.addEventListener('click',() => {
-    currentActive++
-
-    if(currentActive > circles.length){
-        currentActive = circles.length
-    }
-
-    update()
-    
-    console.log(currentActive)
-})
-
-prev.addEventListener('click',() => {
-    currentActive--
-
-    if(currentActive < 1){
-        currentActive = 1
-    }
-
-    update()
-    
-    console.log(currentActive)
-})
-
-
-function update(){
-    circles.forEach((circle,idx)=>{
-        if(idx < currentActive){
-            circle.classList.add('active')
-        }
-        else{
-            circle.classList.remove('active')
-        }
-
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    console.log((actives.length - 1)/ (circles.length - 1)* 100)
-
-    progress.style.width = ((actives.length - 1)/ (circles.length - 1)* 100) + '%'
-
-    if(currentActive === 1){
-        prev.disabled = true
-    }else if(currentActive === circles.length){
-        next.disabled = true
-    }else{
-        prev.disabled = false
-        next.disabled = false
-    }
-}
+close.addEventListener('click',() => container.classList.remove('show-nav'))
