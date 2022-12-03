417c378d6f464e12116b0a183c81310ec8ecf85b
yensan0512 <yensan1254@gmail.com>
2022-12-03 12:34:30 +0800

    'first'

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.container')
+const labels = document.querySelectorAll('.form-control label')
 
-left.addEventListener('mouseenter',() => container.classList.add('hover-left'))
-left.addEventListener('mouseleave',() => container.classList.remove('hover-left'))
-
-right.addEventListener('mouseenter',() => container.classList.add('hover-right'))
-right.addEventListener('mouseleave',() => container.classList.remove('hover-right'))
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter,idx) =>`<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
