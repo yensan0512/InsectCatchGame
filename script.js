d8a0a7344fa89e5c90c84fb3fc4bc79c8a67de78
yensan0512 <yensan1254@gmail.com>
2022-12-03 11:25:38 +0800

    'first'

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const boxes = document.querySelectorAll('.box')
 
-let load = 0
+window.addEventListener('scroll',checkBoxes)
 
-let int = setInterval(blurring,30)
+checkBoxes()
 
-function blurring(){
-    load++
+function checkBoxes(){
+    console.log(window.innerHeight / 5 * 4)
+    const triggerBottom = window.innerHeight / 5 * 4
 
-        if(load > 99){
-            clearInterval(int)
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
         }
-
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load,0,100,1,0)
-    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
-}
-
-const scale = (num, in_min, in_max,out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        else{
+            box.classList.remove('show')
+        }
+    })
 }
