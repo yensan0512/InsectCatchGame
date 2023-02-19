41098be4126a78e8ce5ad8404740eb743d946f87
yensan0512 <yensan1254@gmail.com>
2023-02-19 18:14:53 +0800

    FAQ done

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-const insert = document.getElementById('insert')
+const toggles = document.querySelectorAll('.faq-toggle')
 
-window.addEventListener('keydown',(event) => {
-    insert.innerHTML = `
-    <div class="key">
-    ${event.key === ' ' ? 'Space' : event.key}
-    <small>event.key</small>
-</div>
-
-<div class="key">
-    ${event.keyCode}
-    <small>event.keyCode</small>
-</div>
-
-<div class="key">
-    ${event.code}
-    <small>event.code</small>
-</div>`
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
 })
